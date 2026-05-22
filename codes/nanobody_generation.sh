ce04f1a671d302a0757ae98e16704897cf7aeb49
JLCHONG03 <161560795+JLCHONG03@users.noreply.github.com>
2026-05-22 18:50:07 +0800

    Update nanobody_generation.sh

---
+++ codes/nanobody_generation.sh
@@ -0,0 +1,11 @@
+python sample_antibody_nanobody.py \
+--antigen_pdb /mnt/JL_Chong/CD39_tetramer.pdb \
+--framework_pdb /mnt/JL_Chong/PPIFlow-main/Framework/8coh_nanobody_framework_G.pdb \
+--antigen_chain A --heavy_chain A --specified_hotspots "A138,A139,A140,A141,A142,A143,A144" \
+--cdr_length "CDRH1,8-8,CDRH2,8-8,CDRH3,9-21" \
+--samples_per_target 5 \
+--config /mnt/JL_Chong/PPIFlow-main/configs/inference_nanobody.yaml \
+--model_weights /mnt/JL_Chong/PPIFlow-main/ckpts/nanobody.ckpt \
+--output_dir /mnt/JL_Chong/PPIFlow\
+-main/output/ \
+--name CD39_nanobody
